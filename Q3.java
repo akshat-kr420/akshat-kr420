61c379bb9dffd487a70cff69e6d58b918f2fbace
akshat-kr420 <145797682+akshat-kr420@users.noreply.github.com>
2023-09-22 23:06:50 +0530

    Q3.java

---
+++ Q3.java
@@ -0,0 +1,9 @@
+public class Q3{
+public static void main(String[] args){
+String s="Take you forward";
+String s1="";
+s1=s.replaceAll("[aieou]","");
+System.out.println("String after removing vowels::"+s1);
+
+}
+}
