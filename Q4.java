b630673e93f4fbfb37fc51e4f5e0d1d213a5ceed
akshat-kr420 <145797682+akshat-kr420@users.noreply.github.com>
2023-09-22 23:07:40 +0530

    Q4.java

---
+++ Q4.java
@@ -0,0 +1,8 @@
+public class Q4{
+public static void main(String[] args){
+String s="Take you forward";
+String s1="";
+s1=s.replaceAll(" ","");
+System.out.println("String after removing space::"+s1);
+
+}
