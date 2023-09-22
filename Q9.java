7ed948a9e3fd56df643f744c195681713461161f
akshat-kr420 <145797682+akshat-kr420@users.noreply.github.com>
2023-09-22 23:11:45 +0530

    Q9.java

---
+++ Q9.java
@@ -0,0 +1,29 @@
+public class Q9{
+
+  public static void main(String[] args){     
+      String string = "Google doc";    
+      String word = "", small = "", large="";    
+      String[] words = new String[100];    
+      int length = 0;        
+      string = string + " ";    
+
+      for(int i = 0; i < string.length(); i++){    
+          if(string.charAt(i) != ' '){    
+              word = word + string.charAt(i);    
+          }    
+          else{     
+              words[length] = word;    
+              length++;    
+              word = "";    
+          }    
+      }            
+      small = large = words[0];    
+      for(int k = 0; k < length; k++){    
+          if(small.length() > words[k].length())    
+              small = words[k];    
+          if(large.length() < words[k].length())    
+              large = words[k];    
+      }    
+      System.out.println("Largest word: " + large);    
+  } 
+}  
